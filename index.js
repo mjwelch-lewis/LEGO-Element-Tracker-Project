0a1958659587d78367c5c3972028ca524369c512
mjwelch-lewis <matthewjwelch@lewisu.edu>
2022-06-27 20:52:59 -0500

    Added base files

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const app = express();
 
 app.use(express.static(__dirname + '/client'))
 
+// Start MongoDB Atlas ********
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Username: LEGOElementTrackerUser
+//password: PvIXVCzIOUZMUJ8x
+const mongoose = require("mongoose");
+const mongooseUri = "mongodb+srv://LEGOElementTrackerUser:PvIXVCzIOUZMUJ8x@elementdatabase.gyyim.mongodb.net/elementDatabase"
+
+mongoose.connect(mongooseUri, {useNewUrlParser: true}, {useUnifiedTopology: true})
+
+const elementSchema = {
+	itemID: String,
+	designID: String,
+	description: String,
+	color: String,
+	colorID: String,
+	quantity: Number,
+	imageURL: String,
+}
+
+const Element = mongoose.model("element", elementSchema);
+
 const port = process.env.PORT || 3000
 app.get('/test', function(request, response) {
 	response.type('text/plain')
